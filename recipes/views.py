11e1b215f0a8ed2487173c6d95569b1c0788d3ff
Paulo Samegima <samegima.paulo@gmail.com>
2025-11-19 21:48:25 -0300

    urls.py views.py home.html

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
-
-def contato(request):
-    return HttpResponse('contato')
-
-def sobre(request):
-    return HttpResponse('sobre')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Paulo Samegima',
+    })
